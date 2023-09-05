fcff4564bc3bc8a635cdb8aecc705a650bd40f37
BadarauDavid <badaraudavidiulian@gmail.com>
2023-09-05 21:27:32 +0300

    User Repository and Service

---
+++ Backend/src/main/java/com/trademyskills/service/OfferService.java
@@ -0,0 +1,41 @@
+package com.trademyskills.service;
+
+import com.trademyskills.model.User;
+import com.trademyskills.service.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class OfferService {
+private UserRepository userRepository;
+@Autowired
+    public OfferService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    public void addUser(User user) {
+        userRepository.save(user);
+    }
+
+    public User getUserById(Long id) {
+        return userRepository.getById(id);
+    }
+
+    public void updateUserById(Long id, User updatedUser) {
+        User userFromDb = userRepository.getById(id);
+        userFromDb.setName(userFromDb.getName());
+        userFromDb.setEmail(userFromDb.getEmail());
+        userFromDb.setPhoneNumber(userFromDb.getPhoneNumber());
+        userRepository.save(userFromDb);
+    }
+
+    public void deleteUserById(Long id) {
+        userRepository.deleteById(id);
+    }
+}
---
+++ Backend/src/main/java/com/trademyskills/service/repository/OfferRepository.java
@@ -0,0 +1,9 @@
+package com.trademyskills.service.repository;
+
+import com.trademyskills.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface OfferRepository extends JpaRepository <User,Long> {
+}
