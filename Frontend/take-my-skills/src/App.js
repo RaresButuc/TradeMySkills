c06c9b7c76d9de65d041d96767a0640428fa5365
BadarauDavid <badaraudavidiulian@gmail.com>
2023-09-08 14:55:10 +0300

    ListGroup

--- Frontend/take-my-skills/src/App.js
+++ Frontend/take-my-skills/src/App.js
@@ -1,14 +1,20 @@
 import UserList from "./pages/UserList";
-import './App.css';
+import "bootstrap/dist/css/bootstrap.css";
+import "./App.css";
 import NavBar from "./components/NavBar";
+import ListGroup from "./components/ListGroup";
 
 function App() {
   return (
     <div className="App">
-      <NavBar/>
+      <NavBar />
+      <div className="content-container">
+        <div className="list-group-container">
+          <ListGroup />
+        </div>
+      </div>
     </div>
   );
 }
-
 
 export default App;
