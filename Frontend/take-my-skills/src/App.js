24a61a2443693c234f11a9c32a3ec90092ec3d48
BadarauDavid <badaraudavidiulian@gmail.com>
2023-09-11 15:05:10 +0300

    import bootstrap

--- Frontend/take-my-skills/src/App.js
+++ Frontend/take-my-skills/src/App.js
@@ -1,8 +1,6 @@
 import NavBar from "./components/NavBar";
 import Footer from "./components/Footer";
 import MainPage from "./pages/MainPage";
-import 'bootstrap/dist/css/bootstrap.min.css';
-import "bootstrap/dist/js/bootstrap.min.js"
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./App.css";
 import Login from "./pages/Login";
@@ -12,18 +10,7 @@ import Contact from "./pages/Contact";
 import PostOffer from "./pages/PostOffer";
 
 function App() {
-  // return (
-  //   <>
-  //   <NavBar/>
-  //   <div className="App">
-  //   <Routes>
-  //         <Route path="/" element={<MainPage />} />
-  //         {/* <Route path="/watchlist" element={<Watchlist />} />
-  //         <Route path="/history" element={<History />} /> */}
-  //       </Routes>
-  //     </div>
-  //   </>
-  // );
+
   return (
     <div className="App">
       <NavBar />
