49c416f73f6dbf8ecf257640817877cd41f6bdd1
RaresButuc <eusuntbutuc01@gmail.com>
2023-10-30 12:27:56 +0200

    Creating Register Page and Linking to Backend

---
+++ Frontend/take-my-skills/src/pages/Register.js
@@ -0,0 +1,121 @@
+import { useSignIn } from "react-auth-kit";
+import axios, { AxiosError } from "axios";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+function Register() {
+  const navigate = useNavigate();
+  const [error, setError] = useState("");
+  //   const signIn = useSignIn();
+
+  const onSubmit = async (values) => {
+    console.log("Values: ", values);
+    setError("");
+
+    try {
+      const response = await axios.post(
+        "http://localhost:8080/users/register",
+        values
+      );
+      navigate("/");
+    } catch (err) {
+      if (err instanceof AxiosError) setError(err.response?.data.message);
+      else if (err instanceof Error) setError(err.message);
+    }
+  };
+
+  const onSave = (e) => {
+    e.preventDefault();
+    const formData = new FormData(e.target);
+    const registerData = {
+      name: formData.get("username"),
+      phoneNumber: formData.get("phoneNumber"),
+      email: formData.get("email"),
+      password: formData.get("password"),
+      role: formData.get("role"),
+    };
+    onSubmit(registerData);
+  };
+
+  return (
+    <div className="row" style={{ marginTop: 130 }}>
+      <form onSubmit={onSave}>
+        <div className="mb-3">
+          <label htmlFor="username" className="form-label">
+            Username
+          </label>
+          <input
+            type="text"
+            className="form-control"
+            id="username"
+            name="username"
+          />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="password" className="form-label">
+            Password
+          </label>
+          <input
+            type="password"
+            className="form-control"
+            id="password"
+            name="password"
+          />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="phoneNumber" className="form-label">
+            Phone Number
+          </label>
+          <input
+            type="number"
+            className="form-control"
+            id="phoneNumber"
+            name="phoneNumber"
+          />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="email" className="form-label">
+            Email
+          </label>
+          <input
+            type="email"
+            className="form-control"
+            id="email"
+            name="email"
+          />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="role" className="form-label">
+            Role
+          </label>
+          <select
+            class="form-select"
+            name="role"
+            aria-label="Default select example"
+          >
+            <option selected disabled>
+              Choose A Role
+            </option>
+            <option value="CUSTOMER">Customer</option>
+            <option value="WORKER">Worker</option>
+          </select>
+        </div>
+        <div className="mb-3 form-check">
+          <input
+            type="checkbox"
+            className="form-check-input"
+            id="exampleCheck1"
+          />
+          <label className="form-check-label" htmlFor="exampleCheck1">
+            I accept terms and conditions.
+          </label>
+        </div>
+        <button type="submit" className="btn btn-primary">
+          Sign up
+        </button>
+      </form>
+    </div>
+  );
+}
+
+export default Register;
