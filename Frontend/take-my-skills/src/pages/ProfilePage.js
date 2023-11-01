6bf9fcc0e3ca885b5f1e3a3c22ac5bf393c23262
BadarauDavid <badaraudavidiulian@gmail.com>
2023-11-01 12:24:07 +0200

    get add by user and status- controler

--- Frontend/take-my-skills/src/pages/ProfilePage.js
+++ Frontend/take-my-skills/src/pages/ProfilePage.js
@@ -1,6 +1,30 @@
-import Ads from "../components/Ads";
+import { useAuthUser } from "react-auth-kit";
+import { useEffect } from "react";
+import axios from "axios";
+import React, { useState } from "react";
+
 
 export default function ProfilePage(){
+  const auth = useAuthUser();
+  const [currentUser, setCurrentUser] = useState(null);
+  
+  useEffect(() => {
+    const fetchCurrentUser = async () => {
+      try {
+        const response = await axios.get(`http://localhost:8080/users/email/${auth().email}`);
+        const data = response.data;
+        setCurrentUser(data);
+    
+
+      } catch (err) {
+        console.log(err);
+      }
+    };
+  
+    fetchCurrentUser();
+    
+  }, []);
+  console.log(currentUser);
     return (
         <section >
           <div className="container py-5 mt-5">
@@ -13,8 +37,8 @@ export default function ProfilePage(){
   <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z"/>
   <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z"/>
 </svg>
-                    <h5 className="my-3">John Smith</h5>
-                    <p className="text-muted mb-1">WORKER</p>
+                    <h5 className="my-3">{currentUser.name}</h5>
+                    <p className="text-muted mb-1">{currentUser.role}</p>
                     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"/>
 
 <span class="fa fa-star checked"></span>
@@ -22,7 +46,7 @@ export default function ProfilePage(){
 <span class="fa fa-star checked"></span>
 <span class="fa fa-star"></span>
 <span class="fa fa-star"></span>
-                    <div className="d-flex justify-content-center mb-2">
+                    <div className="d-flex justify-content-center mb-4">
                     </div>
                   </div>
                 </div>
@@ -37,7 +61,7 @@ export default function ProfilePage(){
                         <p className="mb-0">UserName</p>
                       </div>
                       <div className="col-sm-9">
-                        <p className="text-muted mb-0">Johnatan Smith</p>
+                        <p className="text-muted mb-0">{currentUser.name}</p>
                       </div>
                     </div>
                     <hr />
@@ -46,7 +70,7 @@ export default function ProfilePage(){
                         <p className="mb-0">Email</p>
                       </div>
                       <div className="col-sm-9">
-                        <p className="text-muted mb-0">example@example.com</p>
+                        <p className="text-muted mb-0">{currentUser.email}</p>
                       </div>
                     </div>
                     <hr />
@@ -55,17 +79,32 @@ export default function ProfilePage(){
                         <p className="mb-0">Phone</p>
                       </div>
                       <div className="col-sm-9">
-                        <p className="text-muted mb-0">(097) 234-5678</p>
+                        <p className="text-muted mb-0">{currentUser.phoneNumber}</p>
                       </div>
                     </div>
                     </div>
                 </div>
                 <div className="row">
         
+            <div className="d-grid gap-1 ">
+
+            {/* buton cu verde pentru oferte active */}
+                <a
+                  className="btn btn-primary font-weight-bold  mx-2  "
+                  aria-current="page"
+                  href="/id/pending"
+                >
+                  Pending Offer
+                </a>
                
-                <Ads/>
-                 
-              
+                <a
+                  className="btn btn-secondary font-weight-bold  mx-2  "
+                  aria-current="page"
+                  href="/id/completed"
+                >
+                 Completed Offer 
+                </a>
+              </div>
                 </div>
               </div>
             </div>
