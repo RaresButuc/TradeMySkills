7dba74487ab551f0992f52a8e4a2bbe5e33c6f9b
RaresButuc <eusuntbutuc01@gmail.com>
2023-11-16 13:25:10 +0200

    Working on Ad Edit Page

--- Frontend/take-my-skills/src/pages/AdDetails.js
+++ Frontend/take-my-skills/src/pages/AdDetails.js
@@ -11,9 +11,13 @@ export default function AdDetail() {
 
   const [adInfos, setAdInfos] = useState(null);
   const [adLocation, setAdLocation] = useState(null);
-  const [editOrSave, setEditOrSave] = useState(0);
+  const [editOrSave, setEditOrSave] = useState(false);
   const [showEditButtonOrNot, setShowEditButtonOrNot] = useState(false);
   const [buttonValue, setButtonValue] = useState("Edit Ad");
+  // const [possibleStatuses, setossibleStatuses] = useState([
+  //   "PENDING",
+  //   "FINALISED",
+  // ]);
 
   const adTitleRef = useRef("");
   const adDescriptionRef = useRef("");
@@ -41,12 +45,12 @@ export default function AdDetail() {
   };
 
   const changeEdit = () => {
-    if (editOrSave === 0) {
-      setEditOrSave(1);
+    if (editOrSave === false) {
+      setEditOrSave(true);
       setButtonValue("Save");
     } else {
       onSave();
-      setEditOrSave(0);
+      setEditOrSave(false);
       setButtonValue("Edit Profile");
       window.location.reload(false);
     }
@@ -59,6 +63,9 @@ export default function AdDetail() {
         const data = response.data;
         setAdInfos(data);
         setShowEditButtonOrNot(auth().email === data.users[0].email);
+        // if (data.users.length < 2) {
+
+        // }
       } catch (err) {
         console.log(err);
       }
@@ -96,28 +103,92 @@ export default function AdDetail() {
         {/* Titlu */}
         <div className="card container-xl col-8">
           <div className="card-body">
-            <h1>
-              <h1>{adInfos?.title}</h1>
-            </h1>
+            {editOrSave ? (
+              <>
+                <label htmlFor="Title" className="form-label">
+                  Title
+                </label>
+                <br></br>
+                <input
+                  name="title"
+                  ref={adTitleRef}
+                  defaultValue={adInfos?.title}
+                  style={{ maxWidth: 300, height: 50 }}
+                ></input>
+                <div
+                  id="Title-Help"
+                  className="form-text"
+                  style={{ color: "#fa6900" }}
+                >
+                  *Choose a new short and suggestive title
+                </div>
+              </>
+            ) : (
+              <h1>
+                <h1>{adInfos?.title}</h1>
+              </h1>
+            )}
             <hr />
             <div
               className="mt-4 container-xl text-start"
               style={{ minHeight: "52vh" }}
             >
-              <h5>{adInfos?.description}</h5>
+              {editOrSave ? (
+                <>
+                  <label htmlFor="Description" className="form-label">
+                    Description
+                  </label>
+                  <br></br>
+                  <textarea
+                    className="container-xl"
+                    name="description"
+                    ref={adDescriptionRef}
+                    defaultValue={adInfos?.description}
+                    style={{ width: "100%", maxWidth: "500px" }} // Adjust the maximum width as needed
+                  ></textarea>
+                  <div
+                    id="Title-Help"
+                    className="form-text"
+                    style={{ color: "#fa6900" }}
+                  >
+                    *Describe your expectations in detail in no more than 1000
+                    characters
+                  </div>
+                </>
+              ) : (
+                <h5>{adInfos?.description}</h5>
+              )}
             </div>
             {/* Div for Price and Status */}
             <div className="row">
               {/* Price */}
-              <div
-                className="card text-white bg-success container-xl col-3"
-                style={{ height: 90 }}
-              >
-                <div className="card-header">Price</div>
-                <div className="card-body">
-                  <h5 className="card-title">{adInfos?.price}</h5>
+              {editOrSave ? (
+                <div
+                  className="input-group mb-4"
+                  style={{ height: 50, width: 300 }}
+                >
+                  <span className="input-group-text">$</span>
+                  <input
+                    ref={adPriceRef}
+                    type="text"
+                    className="form-control"
+                    aria-label="Amount (to the nearest dollar)"
+                    name="title"
+                    defaultValue={adInfos?.price}
+                  />
+                  <span className="input-group-text">.00</span>
                 </div>
-              </div>
+              ) : (
+                <div
+                  className="card text-white bg-success container-xl col-3"
+                  style={{ height: 90 }}
+                >
+                  <div className="card-header">Price</div>
+                  <div className="card-body">
+                    <h5 className="card-title">{adInfos?.price}</h5>
+                  </div>
+                </div>
+              )}
 
               {showEditButtonOrNot ? (
                 <button
@@ -134,17 +205,37 @@ export default function AdDetail() {
               ) : null}
 
               {/* Status */}
-              <div
-                className={`card text-white bg-${colorDependingOnStatus(
-                  adInfos?.statusOfAd
-                )} container-xl col-3`}
-                style={{ height: 90 }}
-              >
-                <div className="card-header">Status</div>
-                <div className="card-body">
-                  <h5 className="card-title">{adInfos?.statusOfAd}</h5>
+              {editOrSave ? (
+                <div>
+                  <select
+                    className="form-select"
+                    aria-label="select category"
+                    onChange={(e) => setCity(e.target.value)}
+                  >
+                    <option disabled selected>
+                      Select city
+                    </option>
+                    {cities &&
+                      cities.map((city, index) => (
+                        <option value={city.nume} key={index}>
+                          {city.nume}
+                        </option>
+                      ))}
+                  </select>
                 </div>
-              </div>
+              ) : (
+                <div
+                  className={`card text-white bg-${colorDependingOnStatus(
+                    adInfos?.statusOfAd
+                  )} container-xl col-3`}
+                  style={{ height: 90 }}
+                >
+                  <div className="card-header">Status</div>
+                  <div className="card-body">
+                    <h5 className="card-title">{adInfos?.statusOfAd}</h5>
+                  </div>
+                </div>
+              )}
             </div>
           </div>
         </div>
@@ -175,12 +266,12 @@ export default function AdDetail() {
             </div>
           </div>
 
-          <h4 className="mt-2">
+          <h4 className="mt-3">
             {adInfos?.location.nameOfTheCounty},
             {adInfos?.location.nameOfTheCity}
           </h4>
           <iframe
-            className="mb-4"
+            className="mb-3"
             width="400"
             height="200"
             src={`https://www.bing.com/maps/embed/viewer.aspx?v=3&cp=${adLocation?.coordinates[0]}~${adLocation?.coordinates[1]}&lvl=12&w=400&h=200&credentials=AtF5j2AdfXHCqsoqmusG2zXRg7bFR63MIkoMe2EsRAgYfeslufM4-NNWkrfPmywu&form=BMEMJS`}
@@ -194,7 +285,7 @@ export default function AdDetail() {
                 href={`/profile/${id}`}
                 style={{ textDecoration: "none", color: "inherit" }}
               >
-                <div className="row text-center">
+                <div className="container-xl row text-center">
                   <div className="container-xl col-4" style={{ marginTop: 11 }}>
                     <ProfilePhoto width={"75"} height={"75"} />
                   </div>
@@ -212,7 +303,9 @@ export default function AdDetail() {
                 </div>
               </a>
             ) : (
-              <h3>No worker has been selected for this project yet!</h3>
+              <h4 className="container-xl position-relative top-50 start-50 translate-middle">
+                No worker has been selected for this project yet!
+              </h4>
             )}
           </div>
         </div>
